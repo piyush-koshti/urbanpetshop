b1008d5fb28136c29b649efa39d9cccf3fc93dbb
root <root@srv1506563.hstgr.cloud>
2026-04-20 09:21:02 +0000

    add

--- config/server.ts
+++ config/server.ts
@@ -3,10 +3,16 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3035),
-  url: env('PUBLIC_URL', 'http://localhost:3035'),
+
+  // ✅ IMPORTANT FIX
+  url: 'https://cmsurban.indianpetcompany.com',
+
   app: {
     keys: env.array('APP_KEYS'),
   },
+
+  // ✅ VERY IMPORTANT (tells Strapi it's behind nginx)
+  proxy: true,
 });
 
 export default config;
